bf301af0e842b1f9a740193241520678650c7862
SARBANANDA Dev <87931784+sarbanandadev@users.noreply.github.com>
2022-06-11 18:17:35 +0600

    Update getJson.js

--- static/components/utils/getJson.js
+++ static/components/utils/getJson.js
@@ -1,10 +1,32 @@
-const jsonData = require("../../data/mul-00001");
-
 export default function getData() {
-  return JSON.stringify(jsonData[0].data, null, 2);
+  const url = "../../data/mul-00001";
+
+  const loadJSON = (callback) => {
+    const xObj = new XMLHttpRequest();
+    xObj.overrideMimeType("application/json");
+    xObj.open("GET", url, true);
+    xObj.onreadystatechange = () => {
+      if (xObj.readyState === 4 && xObj.status === 200) {
+        callback(xObj.responseText);
+      }
+    };
+    xObj.send(null);
+  };
+
+  const init = () => {
+    loadJSON((response) => {
+      console.log("response =", response);
+      const json = JSON.parse(response);
+      console.log("your local JSON =", JSON.stringify(json, null, 4));
+      const app = document.querySelector("#app");
+      app.innerHTML = `<pre>${JSON.stringify(json, null, 4)}</pre>`;
+    });
+  };
+
+  init();
 }
 
 // const item = data.map((item) => item).join("\n");
 // console.log(item);
 // console.log(getData());
-// console.log(JSON.stringify(jsonData[0].data, null, 2));
+// console.log(JSON.stringify(url[0].data, null, 2));
